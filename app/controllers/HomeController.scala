58057d396febbc383aca0658f2e717acc1ff620b
leisiamedeiros <leisiamedeiros@gmail.com>
2016-10-22 22:25:54 -0300

    customizando views

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 import play.api.i18n.{MessagesApi, I18nSupport}
 
 class HomeController extends Controller {
-  def index = Action {
-    Ok(views.html.index())
+  def index() = Action { implicit request =>
+      Ok(views.html.index())
   }
 }
